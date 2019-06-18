3aaf4b4d3e2fadfee044c25a0d31d231d70c201d
viktorderkach777 <viktorderkach777@yandex.ua>
2019-06-18 20:52:45 +0300

    add form2

--- src/components/animal/add.js
+++ src/components/animal/add.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class AnimalCreate extends Component {
 
@@ -13,16 +14,31 @@ class AnimalCreate extends Component {
     onSubmitForm = (e) => {
         e.preventDefault();
         console.log('----submit form---');
+
+        const model = {
+            name: this.state.name,
+            image: this.state.image
+        };
+        axios.post('https://localhost:44303/api/animal/add', model)
+            .then(
+                (resp) => {
+                    console.log('--success post--', resp.data);
+                    this.props.history.push('/animal');
+                },
+                (err) => {
+                    console.log('--err problem---', err);
+                }
+            );
     }
 
     onChangeInput = (e) => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
         console.log(e.target.name, e.target.value);
     }
 
     render() {
         console.log('---AnimalCreate state----', this.state);
-        const {name, image} = this.state;
+        const { name, image } = this.state;
         return (
             <React.Fragment>
                 <h1>Додати тварину</h1>
@@ -30,12 +46,12 @@ class AnimalCreate extends Component {
                     <div className="form-group">
                         <label htmlFor="name">Назва тварини:</label>
                         <input type="text" className="form-control" name="name"
-                         id="name" value={name} onChange={this.onChangeInput}/>
+                            id="name" value={name} onChange={this.onChangeInput} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="image">Фото:</label>
-                        <input type="text" className="form-control" name="image" 
-                        id="image" value={image} onChange={this.onChangeInput}/>
+                        <input type="text" className="form-control" name="image"
+                            id="image" value={image} onChange={this.onChangeInput} />
                     </div>
                     <button type="submit" className="btn btn-info">Додати</button>
                 </form>
