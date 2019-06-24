b022728722ed8e656f202ce567b98d01a37beb22
Anonymous <user@rivne.itstep.org>
2019-06-24 18:33:12 +0300

    test

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
-
 import rootReducer from "./rootReducer";
 
 const store = createStore(
