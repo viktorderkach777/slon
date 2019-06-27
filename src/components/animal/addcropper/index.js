1ac93fb6308bafed71a2bc5983c3cdfa5a87a81b
viktorderkach777 <viktorderkach777@yandex.ua>
2019-06-27 14:03:36 +0300

    dev first

--- src/components/animal/addcropper/index.js
+++ src/components/animal/addcropper/index.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
+import 'cropperjs/dist/cropper.css';
+import Cropper from 'react-cropper';
 import './index.css';
 
-import CropperWidget from './modal/index';
-import { saveAs } from 'file-saver';
-
+// import CropperWidget from './modal/index';
 
 class AnimalAddCropperWidgetContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
             name: '',
-            image: '',
-            src: 'https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg',
-            isCropped: false
+            image: 'https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg',
+            src: '',
+            isCropped: false,
+            cropResult: null,
+            rotation: 0
         };
+
+        this.rotate = this.rotate.bind(this);
+        this.rotateleft = this.rotateleft.bind(this);
     }
+
+    rotate() {
+        let newRotation = this.state.rotation + 90;
+        if (newRotation >= 360) {
+            newRotation = - 360;
+        }
+        this.setState({
+            rotation: newRotation,
+        })
+    }
+
+    rotateleft() {
+        let newRotation = this.state.rotation - 90;
+        if (newRotation >= 360) {
+            newRotation = - 360;
+        }
+        this.setState({
+            rotation: newRotation,
+        })
+    }
+
     onSubmitForm = (e) => {
         e.preventDefault();
         console.log('----submit form---');
 
-       
-        console.log('handle uploading-', this.state);
-        const fd = new FormData();
-        fd.append('image', this.state.src)
-        const model = { 
+        const model = {
             name: this.state.name,
-            image: this.state.image 
+            image: this.state.image
         };
-        axios.post('http://localhost:100/api/gallery.php', model)
+        axios.post('https://localhost:44303/api/animal/add-base64', model)
             .then(
-                (resp)=>{
+                (resp) => {
                     console.log('--success post--', resp.data);
                     this.props.history.push('/gallery');
                 },
@@ -41,12 +64,12 @@ class AnimalAddCropperWidgetContainer extends Component {
     }
 
     onChangeInput = (e) => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
         console.log(e.target.name, e.target.value);
 
     }
 
-    onSelectImage = () =>{
+    onSelectImage = () => {
         console.log('-------Upload image--------');
         this.inputFileElement.click();
     }
@@ -62,11 +85,10 @@ class AnimalAddCropperWidgetContainer extends Component {
             if (files[0].type.match(/^image\//)) {
                 const reader = new FileReader();
                 reader.onload = () => {
-                    //this.toggle(e);
                     this.setState({ src: reader.result, isCropped: true });
+
                 };
                 reader.readAsDataURL(files[0]);
-                console.log("my photo[0] = ",files[0]);
             }
             else {
                 alert("Невірний тип файлу");
@@ -82,43 +104,87 @@ class AnimalAddCropperWidgetContainer extends Component {
         this.setState({ isCropped: false });
     }
 
-    render() { 
-        const {name, image, src, isCropped }= this.state;
-        console.log("---------src----------", src);
+    cropImage = (e) => {
+        e.preventDefault();
+        if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
+            return;
+        }
+        this.setState({
+            cropResult: this.cropper.getCroppedCanvas().toDataURL(),
+            image: this.cropper.getCroppedCanvas().toDataURL(),
+            isCropped: false
+        });
+    }
+
+    render() {
+        const { name, image, src, isCropped, cropResult, rotation } = this.state;
+
         return (
             <React.Fragment>
-                <h1>Додати фото в галерею кропер</h1> 
+                <h1>Додати фото в галерею кропер</h1>
                 <form onSubmit={this.onSubmitForm}>
                     <div className="form-group">
                         <label htmlFor="name">Назва тварини:</label>
-                        <input type="text" 
-                                className="form-control" 
-                                name="name" 
-                                id="name"
-                                onChange={this.onChangeInput}
-                                value={name} />
+                        <input type="text"
+                            className="form-control"
+                            name="name"
+                            id="name"
+                            onChange={this.onChangeInput}
+                            value={name} />
                     </div>
 
                     <div className="form-group" >
-                        <img className="imgUpload" 
-                            onClick={this.onSelectImage} 
-                            src= {this.state.src} />
+                        <img className="imgUpload"
+                            onClick={this.onSelectImage}
+                            src={this.state.image}
+                            style={{ transform: `rotate(${rotation}deg)` }} />
 
-                        <input ref={input => this.inputFileElement = input} 
-                                type="file" 
-                                onChange={this.onChangeSelecFile} 
-                                className="d-none" />
-
-                       
-                        <CropperWidget loading={isCropped} src={src} onClose={this.onCloseCropper}/>
+                        <input ref={input => this.inputFileElement = input}
+                            type="file"
+                            onChange={this.onChangeSelecFile}
+                            className="d-none" />
                     </div>
                     <button type="submit" className="btn btn-info">Додати</button>
                 </form>
-                
+
+                {/* <img src={this.state.image} /> */}
+
+                <div className={classnames('croppermodal', { 'open': isCropped })}>
+                    <div className="container">
+                        <div className="row" style={{ marginTop: '100px' }}>
+                            <div className="col-md-8">
+                            <input onClick={this.rotateleft} type="button" value="left" />
+                                <Cropper
+                                    style={{ height: 400, width: '100%' }}
+                                    aspectRatio={1 / 1}
+                                    guides={false}
+                                    viewMode={1}
+                                    preview=".img-preview"
+                                    src={src}
+                                    ref={cropper => { this.cropper = cropper; }}
+                                />
+                                <input onClick={this.rotate} type="button" value="right" />
+
+                            </div>
+                            <div className="col-md-4">
+                                <div>
+                                    <div className="box" style={{ width: '100%' }}>
+                                        <button className="btn btn-success" onClick={this.cropImage}>Обрізати фото</button>
+                                        <button className="btn btn-danger" onClick={this.onCloseCropper}>Закрити</button>
+                                        <br />
+                                        <div style={{ marginTop: 20 }}>
+                                            <div className="img-preview" style={{ height: 300, transform: `rotate(${rotation}deg)` }} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
 }
- 
+
 const AnimalAddCropperWidget = AnimalAddCropperWidgetContainer;
 export default AnimalAddCropperWidget;
